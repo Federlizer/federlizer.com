a2c641677d1af9cf8c168406b5e109f335b179d5
Nikola Anastasov Velichkov <federlizer@gmail.com>
2020-05-03 17:55:55 +0200

    Add babel and webpack config

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: path.join(__dirname, '/src/index.jsx'),
+  output: {
+    filename: 'build.js',
+    path: path.join(__dirname, '/dist'),
+  },
+  module: {
+    rules: [{
+      test: /\.jsx$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+    }],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, '/src/index.html'),
+    }),
+  ],
+}
