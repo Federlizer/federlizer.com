e487392c819a352b74c3152b61c8ff7fca16cb6e
Nikola Anastasov Velichkov <federlizer@gmail.com>
2020-05-03 18:04:04 +0200

    Move html template to templates dir

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, '/src/index.html'),
+      template: path.join(__dirname, '/src/templates/index.html'),
     }),
   ],
 }
